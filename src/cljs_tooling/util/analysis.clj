c6b71ff9d14b06a7516a6c217bf3cd0d84721e57
Michael Griffiths <mikey@cich.li>
2014-05-18 15:44:31 +0100

    Add helper fns to analysis.clj for macro support.

--- src/cljs_tooling/util/analysis.clj
+++ src/cljs_tooling/util/analysis.clj
@@ -32,6 +32,42 @@
            (mapcat collect-aliases)
            (into {})))))
 
+(defn ns-aliases
+  "Returns a map of [ns-name-or-alias] to [ns-name] for the given namespace."
+  [env ns]
+  (:requires (find-ns env ns)))
+
+(defn macro-ns-aliases
+  "Returns a map of [macro-ns-name-or-alias] to [macro-ns-name] for the given namespace."
+  [env ns]
+  (:require-macros (find-ns env ns)))
+
+(defn referred-vars
+  "Returns a map of [var-name] to [ns-qualified-var-name] for all referred vars
+  in the given namespace."
+  [env ns]
+  (:uses (find-ns env ns)))
+
+(defn referred-macros
+  "Returns a map of [macro-name] to [ns-qualified-macro-name] for all referred
+  macros in the given namespace."
+  [env ns]
+  (:use-macros (find-ns env ns)))
+
+(defn to-ns
+  "If sym is an alias to, or the name of, a namespace referred to in ns, returns
+  the name of the namespace; else returns nil."
+  [env sym ns]
+  (-> (ns-aliases env ns)
+      (get (u/as-sym sym))))
+
+(defn to-macro-ns
+  "If sym is an alias to, or the name of, a macro namespace referred to in ns,
+  returns the name of the macro namespace; else returns nil."
+  [env sym ns]
+  (-> (macro-ns-aliases env ns)
+      (get (u/as-sym sym))))
+
 (defn- public?
   [var]
   ((complement :private) (val var)))
@@ -41,6 +77,20 @@
   [env ns]
   (let [vars (:defs (find-ns env ns))]
     (into {} (filter public? vars))))
+
+(defn- macro?
+  [var]
+  (-> (val var)
+      meta
+      :macro))
+
+(defn public-macros
+  "Returns a list of the public macros declared in the ns."
+  [ns]
+  (if (and ns (clojure.core/find-ns ns))
+    (->> (ns-publics ns)
+         (filter macro?)
+         (into {}))))
 
 (defn core-vars
   "Returns a list of cljs.core vars visible to the ns."
