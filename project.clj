7cd3852850e1ac6aba97358b939a203473646577
Gary Trakhman <gary.trakhman@gmail.com>
2015-02-22 20:53:09 -0500

    release 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-tooling "0.1.4-SNAPSHOT"
+(defproject cljs-tooling "0.1.4"
   :description "Tooling support for cljs"
   :url "https://github.com/gtrak/cljs-tooling"
   :license {:name "Eclipse Public License"
