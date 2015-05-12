157cb941118e0269e369d1dc5a1bc769fae2ef97
Gary Trakhman <gary.trakhman@gmail.com>
2015-05-12 13:57:22 -0400

    v0.1.7 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-tooling "0.1.6"
+(defproject cljs-tooling "0.1.7"
   :description "Tooling support for cljs"
   :url "https://github.com/gtrak/cljs-tooling"
   :license {:name "Eclipse Public License"
